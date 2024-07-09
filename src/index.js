034688ffa70c9c86576c5a185050c19bd9257644
rozie321 <107440478+rozie321@users.noreply.github.com>
2024-07-09 23:40:35 +0100

    ai updated to js

--- src/index.js
+++ src/index.js
@@ -1,17 +1,25 @@
-
-function displayPoem(event){
-    event.preventDefault();
+function generatePoem(event){
 
     new Typewriter("#poem",{
-        strings:"the poem is fantastic",
+        strings:response.data.answer,
         autoStart: true,
         delay: 1,
         cursor: "",
-
-
     });
 }
 
+function displayPoem(event){
+
+    event.preventDefault();
+    
+    let apiKey = "2046c535afeb092fo82f1d306d8a2b2t";
+    let context =
+    "Your are an artistic AI Assistant that narrates French Poems.The poem must be provided in HTML format. Example: <p>this is a joke</p>";
+    let prompt = "Generate a  four lines short and unique poem in French ";
+    let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+    axios.get(apiUrl).then(generatePoem);   
+}
 
 
 let poemgeneratorElement = document.querySelector('#poem-generator');
