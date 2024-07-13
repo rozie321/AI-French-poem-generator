1672af188346b071a35613f7f51452e25cf7e916
rozie321 <107440478+rozie321@users.noreply.github.com>
2024-07-13 07:28:38 +0100

    UX improvement

--- src/index.js
+++ src/index.js
@@ -12,13 +12,20 @@ function displayPoem(event){
 
     event.preventDefault();
     
+    let instructionsInput=document.querySelector("#user-instructions");
+
     let apiKey = "2046c535afeb092fo82f1d306d8a2b2t";
     let context =
     "Your are an artistic AI Assistant that narrates French Poems.The poem must be provided in HTML format. Example: <p>this is a joke</p>";
-    let prompt = "Generate a  four lines short and unique poem in French ";
+    let prompt = `Generate a  four lines unique and long poem about ${instructionsInput.value}`;
     let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
-    axios.get(apiUrl).then(generatePoem);   
+    let poemElement=document.querySelector("#poem");
+    poemElement.classList.remove("hidden");
+    poemElement.innerHTML=`<div class="blinking-text"> ⏲️Generating a French poem about ${instructionsInput.value}</div>`;
+
+    axios.get(apiUrl).then(generatePoem); 
+
 }
 
 
